b2eabc63363961a09c949c5d54acf1669157b85f
Alex Bates <ajbates93@gmail.com>
2024-03-16 10:33:21 +0000

    feat: user model setup and registration

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -22,6 +22,8 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+
 	// Return the httprouter instance (wrapped in our panic recovery middleware).
 	return app.recoverPanic(app.rateLimit(router))
 }
